75b5437f21536cd545c2b6e77a346faed8c5a08f
Venkat <dvenkat.iam@outlook.com>
2021-01-02 10:22:28 +0200

    Initial commit

---
+++ src/test/java/com/ventoday/SpringBootKafkaProducerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProducerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
